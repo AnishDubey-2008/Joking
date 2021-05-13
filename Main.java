3bd8c799d1afb3a961557ffec9124eb45ffe31d4
Anish-Dev-2008 <>
2021-05-13 11:25:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main(String[	]args)
+  {
+    System.out.println("Hi");
+    Scanner scan = new Scanner(System.in);
+    System. out. println("Welcome to Indian S and A Pubfear");
+    System.out.println( "Please enter your name ");
+    String name = scan.next();
+    System.out.println("DISCLAIMER : \n" +
+                       " na na nana na hut chal bhag nikal " + name );
+    System.out.println("Pagal Ideot " + name );
+    System.out.println(" Phaltu " + name);
+    System.out.println("Khabardar yaha aaya to");
+    System.out.println("Aaya bada game khelne vala chal nickal hutt");
+  }
+}
